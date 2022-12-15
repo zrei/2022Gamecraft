f30878961156e31221ed252da6e2a36b384ddb42
leeroy999 <leeroy.whatsup@rocketmail.com>
2022-12-15 03:11:31 +0100

    fix game manager

--- Blahaj/Assets/Scripts/GameManager.cs
+++ Blahaj/Assets/Scripts/GameManager.cs
@@ -34,12 +34,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
         switch(state)
         {
             case GameState.StartMenu:
                 // Start Menu
-                SceneManager.LoadScene("StartMenu");
+                if (SceneManager.GetActiveScene().name != "StartMenu")
+                {
+                    SceneManager.LoadScene("StartMenu");
+                }
                 break;
             case GameState.NewGame:
                 // New Game
@@ -47,35 +49,53 @@ public class GameManager : MonoBehaviour
                 PlayerStats.ResetStatsEvent();
                 // Reset level
                 level = 1;
-                SceneManager.LoadScene("Level" + level);
+                SceneManager.LoadScene("Game" + level);
+                Time.timeScale = 1f;
+                SetGameState(GameState.InGame);
                 break;
             case GameState.InGame:
                 // Main Game
-                Time.timeScale = 1f;
+                if (Time.timeScale == 0f)
+                {
+                    Time.timeScale = 1f;
+                }
                 break;
             case GameState.NextLevel:
                 // Next Level
                 level++;
-                SceneManager.LoadScene("Level" + level);
+                SceneManager.LoadScene("Game" + level);
+                Time.timeScale = 1f;
+                SetGameState(GameState.InGame);
                 break;
             case GameState.PauseGame:
                 // Pause Game
-                Time.timeScale = 0f;
+                if (Time.timeScale == 1f)
+                {
+                    Time.timeScale = 0f;
+                }
                 break;
             case GameState.DigestionState:
                 // Digestion State
-                SceneManager.LoadScene("DigestionScene");
+                if (SceneManager.GetActiveScene().name != "DigestionScene")
+                {
+                    SceneManager.LoadScene("DigestionScene");
+                }
                 break;
             case GameState.WinGame:
                 Debug.Log("WinGame");
-                SceneManager.LoadScene("WinGame");
+                if (SceneManager.GetActiveScene().name != "WinGame")
+                {
+                    SceneManager.LoadScene("WinGame");
+                }
                 break;
             case GameState.LoseGame:
                 Debug.Log("LoseGame");
-                SceneManager.LoadScene("LoseGame");
+                if (SceneManager.GetActiveScene().name != "LoseGame")
+                {
+                    SceneManager.LoadScene("LoseGame");
+                }
                 break;
         }
-        */
     }
 
     public GameState GetGameState()
